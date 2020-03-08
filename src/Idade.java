b62643b14a800e11a86cecd81ac7ff7e007091e3
Dri Morais <Adrielle>
2020-03-08 17:25:29 -0300

    segundo commit

--- src/Idade.java
+++ src/Idade.java
@@ -2,7 +2,24 @@
 public class Idade {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int idade = 15, resultado1, resultado2;
+		
+		resultado1 = (idade + 5) * 2;
+		resultado2 = resultado1 - 5;
+		
+		System.out.println("A idade é:" + idade);
+		System.out.println("Resultado 1: " + resultado1);
+		System.out.println("Resultado 2:" + resultado2);
+		
+		if(resultado2 > 20) {
+			System.out.println("A idade é maior que 20");
+		}
+		else {
+			System.out.println("A idade é menor que 20");
+		}
+		
+		
+		
 
 	}
 
